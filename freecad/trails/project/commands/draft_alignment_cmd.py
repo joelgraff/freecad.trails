5fae04b577bc5288f485e157b5bd8ae373fca1d9
Joel Graff <monograff76@gmail.com>
2019-04-18 12:25:34 -0500

    initial draw command implementation

--- freecad/trails/project/commands/draft_alignment_cmd.py
+++ freecad/trails/project/commands/draft_alignment_cmd.py
@@ -25,21 +25,23 @@ Command to draft a new alignment
 """
 
 import os
+import uuid
 
 import FreeCAD as App
 import FreeCADGui as Gui
 
 import Draft
 import DraftTools
-from DraftGui import translate, todo
+
+from DraftGui import translate
 
 from ..tasks.alignment.draft_alignment_task import DraftAlignmentTask
-
+from ...project.support import utils
 from ... import resources
 from .alignment_tracker import AlignmentTracker
 
 
-class DraftAlignmentCmd(DraftTools.Line):
+class DraftAlignmentCmd(DraftTools.DraftTool):
     """
     Initiates and manages drawing activities for alignment creation
     """
@@ -49,20 +51,17 @@ class DraftAlignmentCmd(DraftTools.Line):
         Constructor
         """
 
-        super(DraftAlignmentCmd, self).__init__(self)
-        
+        super().__init__()
+
         self.alignment_tracker = None
-        self.point = None
-        self.obj = None
-        self.node = None
-        self.planetrack = None
         self.alignment = None
+        self.alignment_draft = None
         self.temp_group = None
         self.is_activated = False
-
-        DraftTools.Line.Activated(
-            self, name=translate('Transportation', 'Alignment')
-        )
+        self.call = None
+        self.edges = None
+        self.curve_hash = None
+        self.selectable_objects = None
 
     def IsActive(self):
         """
@@ -111,27 +110,53 @@ class DraftAlignmentCmd(DraftTools.Line):
 
         self.is_activated = True
 
-        #DraftTools.Line.Activated(
-        #    self, name=translate('Transportation', 'Alignment')
-        #)
+        DraftTools.DraftTool.Activated(
+            self, name=translate('Transportation', 'Alignment')
+        )
 
+        self.call = self.view.addEventCallback("SoEvent", self.action)
+
+        #make all other visible geometry unselectable
+        view_objects = [
+            _v.ViewObject for _v in App.ActiveDocument.findObjects() 
+            if hasattr(_v, 'ViewObject')
+        ]
+
+        self.selectable_objects = [
+            _v for _v in view_objects if hasattr(_v, 'Selectable')
+        ]
+
+        for vobj in self.selectable_objects:
+            vobj.Selectable = False
+
+        #create temporary group and geometry
         self.temp_group = \
             App.ActiveDocument.addObject('App::DocumentObjectGroup', 'Temp')
 
-        wire = Draft.makeWire(self.alignment.Points)
+        points = self.alignment.Points[:]
 
-        self.temp_group.addObject(wire)
+        for point in points:
+            point.z = 0.1
+
+        self.alignment_draft = utils.make_wire(points, str(uuid.uuid4()))
+
+        self.temp_group.addObject(self.alignment_draft)
 
         if not self.alignment_tracker:
             self.alignment_tracker = AlignmentTracker()
 
         self.alignment.ViewObject.LineColor = (0.5, 0.5, 0.5)
         self.alignment.ViewObject.DrawStyle = u'Dashed'
+        self.alignment.ViewObject.Selectable = False
 
-        panel = DraftAlignmentTask(self.clean_up)
+        self.edges = self.alignment.Proxy.get_edges()
 
-        Gui.Control.showDialog(panel)
+        #panel = DraftAlignmentTask(self.clean_up)
+
+        #Gui.Control.showDialog(panel)
         #panel.setup()
+
+        App.ActiveDocument.recompute()
 
     def select_curve_edges(self, edge_name):
         """
@@ -139,13 +164,102 @@ class DraftAlignmentCmd(DraftTools.Line):
         and the adjacent edges
         """
 
-        curve_dict = self.alignment.Proxy.get_edges()
+        Gui.Selection.clearSelection()
 
-        for _k, _v in curve_dict.items():
+        for _k, _v in self.edges.items():
+
+            #skip tangent selection
+            if len(_v) == 1:
+                continue
 
             if _v.get(edge_name):
 
-                Gui.Selection.addSelection(self.alignment, list(_v.keys()))
+                Gui.Selection.addSelection(
+                    self.alignment_draft, list(_v.keys())
+                )
+
+                return _k
+
+        return None
+
+    def show_control_geometry(self):
+        """
+        Given an edge name, draw the control geometry around the curve
+        """
+
+        control_geo = self.get_control_geometry()
+
+        if control_geo:
+
+            for _geo in control_geo:
+                App.ActiveDocument.getObject(_geo).ViewObject.Visibility = True
+
+            return
+
+        curve = self.alignment.Proxy.get_geometry(self.curve_hash)
+
+        if not curve:
+            return
+
+        wires = {
+            'rad_in_':  [curve['Center'], curve['Start']],
+            'rad_out_':    [curve['Center'], curve['End']],
+            'tan_in_':  [curve['PI'], curve['Start']],
+            'tan_out_':    [curve['PI'], curve['End']]
+        }
+
+        for _k, _v in wires.items():
+
+            _wire = utils.make_wire(_v, wire_name=_k + str(self.curve_hash))
+
+            _wire.ViewObject.DrawStyle = 'Dashed'
+            _wire.ViewObject.LineColor = (0.8, 0.8, 0.1)
+            _wire.ViewObject.Selectable = False
+
+            self.temp_group.addObject(_wire)
+
+    def get_control_geometry(self):
+        """
+        Return a list of control geometry names
+        """
+
+        if not self.curve_hash:
+            return None
+
+        hash_str = str(self.curve_hash)
+
+        return [_v.Name for _v in self.temp_group.Group if hash_str in _v.Name]
+
+    def activate_control_geometry(self):
+        """
+        Set the view attributes to indicate selected curve is in edit mode
+        """
+
+        for _nam in self.get_control_geometry():
+
+            vobj = App.ActiveDocument.getObject(_nam).ViewObject
+            vobj.DrawStyle = u'Solid'
+            vobj.LineColor = (0.0, 0.0, 1.0)
+
+    def hide_control_geometry(self):
+        """
+        Hide defined geometry so we don't have to recreate it later
+        """
+
+        control_geo = self.get_control_geometry()
+
+        for _geo in control_geo:
+            App.ActiveDocument.getObject(_geo).ViewObject.Visibility = False
+
+    def delete_control_geometry(self):
+        """
+        Delete the control geometry for the current curve
+        """
+
+        for _nam in self.get_control_geometry():
+
+            print('deleting ', _nam)
+            App.ActiveDocument.removeObject(_nam)
 
     def action(self, arg):
         """
@@ -161,16 +275,34 @@ class DraftAlignmentCmd(DraftTools.Line):
         #trap mouse movement
         if arg['Type'] == 'SoLocation2Event':
 
-            p = Gui.ActiveDocument.ActiveView.getCursorPos()
-            point = Gui.ActiveDocument.ActiveView.getPoint(p)
-            info = Gui.ActiveDocument.ActiveView.getObjectInfo(p)
+            _p = Gui.ActiveDocument.ActiveView.getCursorPos()
+            #point = Gui.ActiveDocument.ActiveView.getPoint(p)
+            info = Gui.ActiveDocument.ActiveView.getObjectInfo(_p)
+
+            curve_hash = None
 
             if info:
-                if 'Edge' in info['Component']:
-                    self.select_curve_edges(info['Component'])
+                if info['Object'] == self.alignment_draft.Name:
+                    curve_hash = self.select_curve_edges(info['Component'])
+
+            if curve_hash:
+
+                if self.curve_hash != curve_hash:
+                    self.curve_hash = curve_hash
+                    self.show_control_geometry()
+
+            else:
+
+                if Gui.Selection.getSelection():
+                    Gui.Selection.clearSelection()
+
+                if self.curve_hash:
+                    self.hide_control_geometry()
+                    self.curve_hash = None
 
         #    self.alignment_tracker.update(self.node + [self.point])
 
+            App.ActiveDocument.recompute()
             DraftTools.redraw3DView()
 
             return
@@ -198,20 +330,20 @@ class DraftAlignmentCmd(DraftTools.Line):
                     self.node.append(self.point)
                     self.draw_update(self.point)
 
-                    if not self.isWire and len(self.node) == 2:
-                        self.finish(False, cont=True)
+                    #if not self.isWire and len(self.node) == 2:
+                    #    self.finish(False, cont=True)
 
     def undo_last(self):
         """
         Undo the last segment
         """
 
-        if len(self.node) > 1:
+        #if len(self.node) > 1:
 
-            self.node.pop()
-            self.alignment_tracker.update(self.node)
-            self.obj.Shape = self.update_shape(self.node)
-            print(translate('Transporation', 'Undo last point'))
+        #    self.node.pop()
+        #    self.alignment_tracker.update(self.node)
+        #    self.obj.Shape = self.update_shape(self.node)
+        #    print(translate('Transporation', 'Undo last point'))
 
     def draw_update(self, point):
         """
@@ -231,7 +363,7 @@ class DraftAlignmentCmd(DraftTools.Line):
 
         print(type(self.obj.Shape))
         #res = self.update_shape(self.node)
-        print(type(res))
+        #print(type(res))
         #self.obj.Shape = self.update_shape(self.node)
 
         print(
@@ -302,13 +434,18 @@ class DraftAlignmentCmd(DraftTools.Line):
         if self.alignment:
             self.alignment.ViewObject.LineColor = (0.0, 0.0, 0.0)
             self.alignment.ViewObject.DrawStyle = u'Solid'
+            self.alignment.ViewObject.Selectable = True
 
         if self.temp_group:
-            if self.temp_group.OutList:
-                for _geo in self.temp_group.OutList:
-                    self.temp_group.removeObject(_geo)
+            for _v in self.temp_group.Group:
+                App.ActiveDocument.removeObject(_v.Name)
 
             App.ActiveDocument.removeObject(self.temp_group.Name)
+
+        #restore selectability to objects that had it
+        if self.selectable_objects:
+            for vobj in self.selectable_objects:
+                vobj.Selectable = True
 
         self.is_activated = False
 
