d0f137befb070f0ba4e5a81fcd68bfb51bce384b
Joel Graff <monograff76@gmail.com>
2019-05-23 15:39:46 -0500

    begin implement for alignment tracker connection group

--- freecad/trails/project/trackers/alignment_tracker.py
+++ freecad/trails/project/trackers/alignment_tracker.py
@@ -46,12 +46,17 @@ class AlignmentTracker(BaseTracker):
         _names = [doc.Name, object_name, 'ALIGNMENT_TRACKER']
         super().__init__(names=_names, select=False, group=True)
 
-    def get_alignment_group(self, pi_coord):
+    def get_selection_group(self, pi_coords):
         """
         Return a node of coordinates and lines which represents the
         discretized alignment from the end of the curve at the specified
         PI coordinate to the end of the alignment
         """
+
+        if len(pi_coords) < 2:
+            return coin.SoGroup()
+
+        pi_coord = pi_coords[0]
 
         _start_sta = 0.0
 
@@ -79,6 +84,66 @@ class AlignmentTracker(BaseTracker):
         _group.addChild(_line)
 
         return _group
+
+    def get_connection_group(self, selected):
+        """
+        Build the connection group based on passed selected PI's
+        """
+
+        if len(selected) < 1:
+            return coin.SoGroup()
+
+        _pi_start = selected[0]
+        _curves = [{}]
+
+        for _curve in self.model.data['geometry']:
+
+            _pi = _curve.get('PI')
+
+            if _pi is None:
+                continue
+
+            #if the start curve is selected, save the next curve and break
+            #(only one PI is selected)
+            if len(_curves) == 2:
+                _curves.append(_curve)
+                break
+
+            #save the start curve and break if more than one PI is selected
+            if support.within_tolerance(_pi, _pi_start, 0.1):
+                _curves.append(_curve)
+
+                if len(selected) > 1:
+                    break
+
+            #save the pi as the previous curve until the start curve is found
+            if len(_curves) == 1:
+                _curves[0] = _curve
+
+        _start_sta = _curves[0]['InternalStation'][0]
+        _end_sta = _curves[-1]['InternalStation'][1]
+
+        _coords = self.model.discretize_geometry([_start_sta, _end_sta])
+
+        _group = coin.SoGroup()
+        _group.setName('ALIGNMENT_TRACKER')
+
+        _coord = coin.SoCoordinate3()
+        _coord.point.setValues(0, len(_coords), [list(_v) for _v in _coords])
+
+        _line = coin.SoLineSet()
+
+        _group.addChild(_coord)
+        _group.addChild(_line)
+
+        return _group
+
+    def drag_callback(self, xform, pos):
+        """
+        Callback for drag operations
+        """
+
+        pass
 
     def update(self, points):
         """
