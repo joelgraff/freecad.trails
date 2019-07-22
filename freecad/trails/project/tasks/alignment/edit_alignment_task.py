bf1609c8c65bdeed7961f9f034cf50d66d261321
Joel Graff <monograff76@gmail.com>
2019-07-22 09:40:56 -0500

    minor fixes to smooth zoom

--- freecad/trails/project/tasks/alignment/edit_alignment_task.py
+++ freecad/trails/project/tasks/alignment/edit_alignment_task.py
@@ -179,10 +179,15 @@ class EditAlignmentTask:
         #calculate a total change value
         _pct_chg = abs(_height - _start_ht) / (_height + _start_ht)
 
-        #at 50% change or more, use 60 frames, otherwise scale frames
+        #at 50% change or more, use 60 frames,
+        #otherwise scale frames to a minimum of 10 frames
         if _pct_chg < 0.5:
             _frames *= _pct_chg * 2.0
 
+            if _frames < 10.0:
+                _frames = 10.0
+
+        #build cosine-based animation curve and reverse
         _steps = [
             math.cos((_i/_frames) * (math.pi/2.0)) * _frames\
                 for _i in range(0, int(_frames))
@@ -190,6 +195,7 @@ class EditAlignmentTask:
 
         _steps = _steps[::-1]
 
+        #calculate position and height deltas for transition loop
         _d_pos = _center - _start_pos
         _d_pos.multiply(1.0 / _frames)
 
