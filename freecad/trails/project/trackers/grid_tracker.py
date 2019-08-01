228e99a92626030cbdebf9099839984c8c021ef1
Joel Graff <monograff76@gmail.com>
2019-08-01 09:08:12 -0500

    grid tracker test

---
+++ freecad/trails/project/trackers/grid_tracker.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+#**************************************************************************
+#*                                                                     *
+#* Copyright (c) 2019 Joel Graff <monograff76@gmail.com>               *
+#*                                                                     *
+#* This program is free software; you can redistribute it and/or modify*
+#* it under the terms of the GNU Lesser General Public License (LGPL)  *
+#* as published by the Free Software Foundation; either version 2 of   *
+#* the License, or (at your option) any later version.                 *
+#* for detail see the LICENCE text file.                               *
+#*                                                                     *
+#* This program is distributed in the hope that it will be useful,     *
+#* but WITHOUT ANY WARRANTY; without even the implied warranty of      *
+#* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the       *
+#* GNU Library General Public License for more details.                *
+#*                                                                     *
+#* You should have received a copy of the GNU Library General Public   *
+#* License along with this program; if not, write to the Free Software *
+#* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307*
+#* USA                                                                 *
+#*                                                                     *
+#***********************************************************************
+"""
+Grid tracker
+"""
+
+from pivy import coin
+
+from .base_tracker import BaseTracker
+
+from ..support.drag_state import DragState
+
+class GridTracker(BaseTracker):
+    """
+    GridTracker Class
+
+    self.points - list of Vectors
+    self.selction_nodes - 
+        list of point indices which correspond to node trackers
+    """
+
+    def __init__(self, names, nodes=None):
+        """
+        Constructor
+        """
+
+        self.line = coin.SoLineSet()
+        self.name = names[2]
+        self.coord = coin.SoCoordinate3()
+        self.points = None
+        self.selection_nodes = None
+        self.selection_indices = []
+
+        self.group = coin.SoSeparator()
+        self.drag_coord = None
+        self.drag_idx = None
+        self.drag_start = []
+
+        if not nodes:
+            nodes = []
+
+        elif not isinstance(nodes, list):
+            nodes = list(nodes)
+
+        nodes += [self.coord, self.line]
+
+        super().__init__(names=names, children=nodes)
