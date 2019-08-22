87aa73184a6abb7f2f781476ec1593dda1e39622
Joel Graff <monograff76@gmail.com>
2019-08-22 09:05:02 -0500

    reimplemented alignment drag ops

--- freecad/trails/project/trackers/alignment_tracker.py
+++ freecad/trails/project/trackers/alignment_tracker.py
@@ -113,30 +113,21 @@ class AlignmentTracker(BaseTracker):
             MouseState().component + ' ' + str(tuple(MouseState().coordinates))
         )
 
-    def start_drag(self):
+    def on_drag(self):
         """
         Override base function
         """
 
         pass
 
-    def on_drag(self):
-        """
-        Override base function
-        """
-
         if not MouseState().button1.dragging:
             return
-
-        print(self.name, 'drag curves = ', self.drag_curves)
 
         #get the list of tangents
         _tans = [0.0] \
             + [_v.drag_arc['Tangent'] for _v in self.drag_curves] + [0.0]
 
-        print('tangents', _tans)
-        #enumerate all but the last tangent,
-        #getting the curve one in
+        #validate the drag curves
         for _i, _v in enumerate(_tans[:-2]):
             self.drag_curves[_i].validate(lt_tan=_v, rt_tan=_tans[_i + 2])
 
@@ -145,7 +136,6 @@ class AlignmentTracker(BaseTracker):
         Override base fucntion
         """
 
-        print(self.name, 'end drag')
         self.drag_curves = []
 
     def button_event(self, arg):
@@ -153,65 +143,19 @@ class AlignmentTracker(BaseTracker):
         Override base button actions
         """
 
-        #multi-select
-        _pick = MouseState().component
-
-        if not _pick:
+        #abort if not multi-selecting or button up
+        if not MouseState().ctrlDown or MouseState().button1.state == 'UP':
             return
 
-        if MouseState().button1.state == 'UP':
+        #abort if a node isn't picked
+        if not 'NODE' in MouseState().component:
             return
 
-        _i = 0
+        #get the nodes we need to select, and select them
+        _idx = int(MouseState().component.split('-')[1]) + 1
 
-        #abort if nodes are selected - this routine is only for multi-selecting
-        for _v in self.trackers['Nodes']:
-
-            if _v.is_selected():
-                _i += 1
-
-        if MouseState().ctrlDown and _i > 1:
-            return
-
-        elif _i > 0:
-            return
-
-        #if a curve is picked, set the visibility ignore flag on, so the
-        #curve PI doesn't get redrawn
-        #if 'CURVE' in _pick:
-
-        #    _idx = int(_pick.split('-')[1])
-
-        #    self.trackers['Nodes'][_idx + 1].state.visible.ignore = True
-
-        if 'NODE' in _pick:
-
-            print(self.name, 'button', MouseState().component)
-
-            _idx = int(_pick.split('-')[1])
-
-            _nodes = [self.trackers['Nodes'][_idx]]
-
-            if MouseState().ctrlDown:
-                _nodes = self.trackers['Nodes'][_idx:]
-
-            for _v in _nodes:
-                SelectState()._selected.append(_v)
-                _v.state.selected.ignore_once()
-
-            _lower = max(0, _idx - 2)
-
-            _max = 1
-
-            if MouseState().ctrlDown:
-                _max = 0
-
-            _upper = min(len(self.trackers['Curves']), _idx + _max)
-
-            print('picking curves ', _lower, _upper)
-
-            for _i in range(_lower, _upper):
-                self.drag_curves.append(self.trackers['Curves'][_i])
+        for _v in self.trackers['Nodes'][_idx:]:
+            SelectState().select(_v, force=True)
 
     def build_trackers(self):
         """
@@ -223,11 +167,8 @@ class AlignmentTracker(BaseTracker):
 
         #build a list of coordinates from curves in the geometry
         _nodes = [Vector()]
-
-        for _geo in _model.get('geometry'):
-
-            if _geo.get('Type') != 'Line':
-                _nodes += [_geo.get('PI')]
+        _nodes += [_v.get('PI')\
+            for _v in _model.get('geometry') if _v.get('Type') != 'Line']
 
         _nodes += [_model.get('meta').get('End')]
 
@@ -238,9 +179,7 @@ class AlignmentTracker(BaseTracker):
         #node trackers
         for _i, _pt in enumerate(_nodes):
 
-            _tr = NodeTracker(
-                names=_names[:2] + ['NODE-' + str(_i)], point=_pt
-            )
+            _tr = NodeTracker(names=_names + ['NODE-' + str(_i)], point=_pt)
             _result['Nodes'].append(_tr)
 
         _result['Nodes'][0].is_end_node = True
@@ -251,19 +190,17 @@ class AlignmentTracker(BaseTracker):
 
             _nodes = _result['Nodes'][_i:_i + 2]
 
-            _result['Tangents'].append(
-                self._build_wire_tracker(
-                    wire_name=_names[:2] + ['WIRE-' + str(_i)],
-                    nodes=_nodes,
-                    points=[],
-                    select=False
-                )
-            )
+            _wt = WireTracker(names=_names + ['WIRE-' + str(_i)])
 
-        _curves = self.alignment.get_curves()
-        _names = self.names[:2]
+            _wt.set_selectability(False)
+            _wt.set_points(nodes=_nodes)
+            _wt.update()
+
+            _result['Tangents'].append(_wt)
 
         #curve trackers
+        _curves = self.alignment.get_curves()
+
         for _i in range(0, len(_result['Tangents']) - 1):
 
             _ct = CurveTracker(
@@ -279,26 +216,12 @@ class AlignmentTracker(BaseTracker):
 
         self.trackers = _result
 
-    def _build_wire_tracker(self, wire_name, nodes, points, select=False):
-        """
-        Convenience function for WireTracker construction
-        """
-
-        _wt = WireTracker(names=wire_name)
-
-        _wt.set_selectability(select)
-        _wt.set_points(points, nodes)
-        _wt.update()
-
-        return _wt
-
     def finalize(self):
         """
         Cleanup the tracker
         """
 
         for _t in self.trackers.values():
-
             for _u in _t:
                 _u.finalize()
 
