ad700c4a7b51578d9e1df9b1aecb1b566b07c77a
Joel Graff <monograff76@gmail.com>
2019-10-16 14:27:33 -0500

    import fix

--- freecad/trails/project/tasks/alignment/base_tracker_test_task.py
+++ freecad/trails/project/tasks/alignment/base_tracker_test_task.py
@@ -31,9 +31,9 @@ import FreeCADGui as Gui
 
 import DraftTools
 
-from pivy_trackers.tracker_tester import TrackerTester
+from pivy_trackers.tracker.tracker_tester import TrackerTester
 from pivy_trackers.support.view_state import ViewState
-from pivy_trackers.task_tracker import TaskTracker
+from pivy_trackers.tracker.task_tracker import TaskTracker
 
 from ....alignment import alignment
 from ...support import const
