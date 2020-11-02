4a1182366a16e6289cb657469b4ccb0c82a65859
HakanSeven12 <hakanseven12@gmail.com>
2020-11-02 17:44:49 +0300

    [PG] Color and size added

--- freecad/trails/geomatics/point/point_group.py
+++ freecad/trails/geomatics/point/point_group.py
@@ -3,56 +3,115 @@
 import FreeCAD, FreeCADGui
 from pivy import coin
 from ...design.project.support import utils
+from freecad.trails import ICONPATH
+
+
+
+def create(points, name='Point_group'):
+    obj=FreeCAD.ActiveDocument.addObject("App::FeaturePython", name)
+    PointGroup(obj)
+    obj.Points = points
+    ViewProviderPointGroup(obj.ViewObject)
+
 
 class PointGroup:
+    """
+    This class is about Point Group Object data features.
+    """
+
     def __init__(self, obj):
-        '''Add some custom properties to our box feature'''
-        obj.addProperty("App::PropertyVectorList", "Points", "Base", "List of group points").Points = ()
+        '''
+        Set data properties.
+        '''
+
+        obj.addProperty(
+            "App::PropertyVectorList",
+            "Points",
+            "Base",
+            "List of group points").Points = ()
+
         obj.Proxy = self
         self.Points = None
    
     def onChanged(self, fp, prop):
-        '''Do something when a property has changed'''
-        FreeCAD.Console.PrintMessage("Change property: " + str(prop) + "\n")
+        '''
+        Do something when a data property has changed.
+        '''
+
+        return
  
     def execute(self, fp):
-        '''Do something when doing a recomputation, this method is mandatory'''
-        FreeCAD.Console.PrintMessage("Recompute Python Box feature\n")
+        '''
+        Do something when doing a recomputation. 
+        '''
+
+        return
+
 
 class ViewProviderPointGroup:
+    """
+    This class is about Point Group Object view features.
+    """
+
     def __init__(self, obj):
-        '''Set this object to the proxy object of the actual view provider'''
-        obj.addProperty("App::PropertyColor","Color","Box","Color of the box").Color=(1.0,0.0,0.0)
+        '''
+        Set view properties.
+        '''
+
+        obj.addProperty(
+            "App::PropertyColor",
+            "PointColor",
+            "Point Style",
+            "Color of the point group").PointColor=(1.0,0.0,0.0)
+
+        obj.addProperty(
+            "App::PropertyFloatConstraint",
+            "PointSize",
+            "Point Style",
+            "Size of the point group").PointSize=(2.0)
+
         obj.Proxy = self
+        obj.PointSize = (2.0,1.0,64.0,1.0)
  
     def attach(self, obj):
-        '''Setup the scene sub-graph of the view provider, this method is mandatory'''
-        geo_system, geo_coord = utils.get_geo(coords=obj.Object.Points[0])
-        self.coord = coin.SoGeoCoordinate()
-        self.coord.geoSystem.setValues(geo_system)
-        self.coord.point.values = obj.Object.Points
-        data = coin.SoPointSet()
+        '''
+        Create 3D view.
+        '''
 
-        self.sep = coin.SoGeoSeparator()
-        self.sep.geoSystem.setValues(geo_system)
-        self.sep.geoCoords.setValue(geo_coord[0], geo_coord[1], geo_coord[2])
+        # Get geo system and geo origin.
+        geo_system, geo_origin = utils.get_geo(coords=obj.Object.Points[0])
 
-        self.shaded = self.sep
-        self.wireframe = self.sep
-        self.color = coin.SoBaseColor()
-       
-        self.shaded.addChild(self.color)
-        self.shaded.addChild(self.coord)
-        self.shaded.addChild(data)
-        obj.addDisplayMode(self.shaded,"Shaded")
-        style=coin.SoDrawStyle()
-        style.style = coin.SoDrawStyle.LINES
-        self.wireframe.addChild(style)
-        self.wireframe.addChild(self.color)
-        self.wireframe.addChild(self.coord)
-        self.wireframe.addChild(data)
-        obj.addDisplayMode(self.wireframe,"Wireframe")
-        self.onChanged(obj,"Color")
+        # Geo coordinates.
+        geo_coords = coin.SoGeoCoordinate()
+        geo_coords.geoSystem.setValues(geo_system)
+        geo_coords.point.values = obj.Object.Points
+
+        # Geo Seperator.
+        geo_seperator = coin.SoGeoSeparator()
+        geo_seperator.geoSystem.setValues(geo_system)
+        geo_seperator.geoCoords.setValue(geo_origin[0], geo_origin[1], geo_origin[2])
+
+        # Point group features.
+        points = coin.SoPointSet()
+        self.point_color = coin.SoBaseColor()
+        self.point_style = coin.SoDrawStyle()
+        self.point_style.style = coin.SoDrawStyle.POINTS
+
+        # Highlight for selection.
+        highlight = coin.SoType.fromName('SoFCSelection').createInstance()
+        highlight.addChild(geo_coords)
+        highlight.addChild(points)
+
+        # Point group root.
+        point_root = geo_seperator
+        point_root.addChild(self.point_color)
+        point_root.addChild(self.point_style)
+        point_root.addChild(highlight)
+        obj.addDisplayMode(point_root,"Point")
+
+        # Take features from properties.
+        self.onChanged(obj,"PointSize")
+        self.onChanged(obj,"PointColor")
  
     def updateData(self, fp, prop):
         '''If a property of the handled feature has changed we have the chance to handle this here'''
@@ -66,14 +125,19 @@ class ViewProviderPointGroup:
         pass
  
     def getDisplayModes(self,obj):
-        '''Return a list of display modes.'''
+        '''
+        Return a list of display modes.
+        '''
+
         modes=[]
-        modes.append("Shaded")
-        modes.append("Wireframe")
+        modes.append("Point")
         return modes
  
     def getDefaultDisplayMode(self):
-        '''Return the name of the default display mode. It must be defined in getDisplayModes.'''
+        '''
+        Return the name of the default display mode. It must be defined in getDisplayModes.
+        '''
+
         return "Shaded"
  
     def setDisplayMode(self,mode):
@@ -83,41 +147,21 @@ class ViewProviderPointGroup:
  
     def onChanged(self, vp, prop):
         '''Here we can do something when a single property got changed'''
-        FreeCAD.Console.PrintMessage("Change property: " + str(prop) + "\n")
-        if prop == "Color":
-            c = vp.getPropertyByName("Color")
-            self.color.rgb.setValue(c[0],c[1],c[2])
+
+        if prop == "PointSize":
+            c = vp.getPropertyByName("PointSize")
+            self.point_style.pointSize.setValue(c)
+
+        if prop == "PointColor":
+            c = vp.getPropertyByName("PointColor")
+            self.point_color.rgb.setValue(c[0],c[1],c[2])
  
     def getIcon(self):
-        '''Return the icon in XPM format which will appear in the tree view. This method is\
-                optional and if not defined a default icon is shown.'''
-        return """
-            /* XPM */
-            static const char * ViewProviderBox_xpm[] = {
-            "16 16 6 1",
-            "   c None",
-            ".  c #141010",
-            "+  c #615BD2",
-            "@  c #C39D55",
-            "#  c #000000",
-            "$  c #57C355",
-            "        ........",
-            "   ......++..+..",
-            "   .@@@@.++..++.",
-            "   .@@@@.++..++.",
-            "   .@@  .++++++.",
-            "  ..@@  .++..++.",
-            "###@@@@ .++..++.",
-            "##$.@@$#.++++++.",
-            "#$#$.$$$........",
-            "#$$#######      ",
-            "#$$#$$$$$#      ",
-            "#$$#$$$$$#      ",
-            "#$$#$$$$$#      ",
-            " #$#$$$$$#      ",
-            "  ##$$$$$#      ",
-            "   #######      "};
-            """
+        '''
+        Return object treeview icon.
+        '''
+
+        return ICONPATH + '/icons/PointGroup.svg'
  
     def __getstate__(self):
         '''When saving the document this object gets stored using Python's json module.\
@@ -129,10 +173,3 @@ class ViewProviderPointGroup:
         '''When restoring the serialized object from document we have the chance to set some internals here.\
                 Since no data were serialized nothing needs to be done here.'''
         return None
-
-
-def create(points, name='Point_group'):
-    obj=FreeCAD.ActiveDocument.addObject("App::FeaturePython", name)
-    PointGroup(obj)
-    obj.Points = points
-    ViewProviderPointGroup(obj.ViewObject)
