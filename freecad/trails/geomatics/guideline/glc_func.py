8cff65559a2d6a7770cf5c602f4fb101418321ac
Hakan Seven <hakanseven12@gmail.com>
2021-02-25 14:18:37 +0300

    new gl folder

---
+++ freecad/trails/geomatics/guideline/glc_func.py
@@ -0,0 +1,121 @@
+
+# /**********************************************************************
+# *                                                                     *
+# * Copyright (c) 2021 Hakan Seven <hakanseven12@gmail.com>             *
+# *                                                                     *
+# * This program is free software; you can redistribute it and/or modify*
+# * it under the terms of the GNU Lesser General Public License (LGPL)  *
+# * as published by the Free Software Foundation; either version 2 of   *
+# * the License, or (at your option) any later version.                 *
+# * for detail see the LICENCE text file.                               *
+# *                                                                     *
+# * This program is distributed in the hope that it will be useful,     *
+# * but WITHOUT ANY WARRANTY; without even the implied warranty of      *
+# * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the       *
+# * GNU Library General Public License for more details.                *
+# *                                                                     *
+# * You should have received a copy of the GNU Library General Public   *
+# * License along with this program; if not, write to the Free Software *
+# * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307*
+# * USA                                                                 *
+# *                                                                     *
+# ***********************************************************************
+
+'''
+Define Surface Object functions.
+'''
+
+import FreeCAD
+import Part
+import copy
+
+class GLCFunc:
+    """
+    This class is contain Surface Object functions.
+    """
+    def __init__(self):
+        pass
+
+    def get_alignment_infos(self, alignment):
+        if hasattr(alignment.Proxy, 'model'):
+            start = alignment.Proxy.model.data['meta']['StartStation']*1000
+            length = alignment.Proxy.model.data['meta']['Length']
+            end = start + length
+        else:
+            start = 0.0
+            length = alignment.Length.Value
+            end = start + length
+        return start, end
+
+    def generate(self, alignment, increments, region, horiz_pnts = True):
+        """
+        Generates guidelines along a selected alignment
+        """
+        # Guideline intervals
+        tangent_increment = increments[0]/1000
+        curve_increment = increments[1]/1000
+        spiral_increment = increments[2]/1000
+
+        # Region limits
+        start_station = round(region[0]/1000, 3)
+        end_station = round(region[1]/1000, 3)
+
+        # Retrieve alignment data get geometry and placement
+        stations = []
+        if hasattr(alignment.Proxy, 'model'):
+            start = alignment.Proxy.model.data['meta']['StartStation']
+            length = alignment.Proxy.model.data['meta']['Length']
+            end = start + length/1000
+
+            geometry = alignment.Proxy.model.data['geometry']
+            for element in geometry:
+                # Get starting and ending stations based on alignment
+                elem_start = element.get('StartStation')
+                elem_end = element.get('StartStation')+element.get('Length')/1000
+
+                if elem_start != 0 and horiz_pnts:
+                        stations.append(elem_start)
+
+                # Generate line intervals
+                if element.get('Type') == 'Line':
+
+                    # Iterate the station range
+                    for sta in range(int(elem_start), int(elem_end)):
+
+                        # Add stations which land on increments exactly
+                        if sta % int(tangent_increment) == 0:
+                            stations.append(sta)
+
+                # Generate curve intervals
+                elif element.get('Type') == 'Curve':
+                    for sta in range(int(elem_start), int(elem_end)):
+                        if sta % int(curve_increment) == 0:
+                            stations.append(sta)
+
+                #Generate spiral intervals
+                elif element.get("Type") == 'Spiral':
+                    for sta in range(int(elem_start), int(elem_end)):
+                        if sta % int(spiral_increment) == 0:
+                            stations.append(sta)
+
+            # Add the end station
+            stations.append(round(end,3))
+
+        # Create guide lines from standart line object
+        else:
+            length = alignment.Length.Value
+            for sta in range(0, int(length/1000)):
+                if sta % int(tangent_increment) == 0:
+                    stations.append(sta)
+
+            # Add the end station
+            stations.append(round(length/1000,3))
+
+        # Iterate the stations, appending what falls in the specified limits
+        region_stations = []
+        for sta in stations:
+            if start_station <= sta <= end_station:
+                region_stations.append(sta)
+
+        region_stations.sort()
+        return region_stations
---
+++ freecad/trails/geomatics/guideline/guidelines.py
@@ -0,0 +1,236 @@
+# /**********************************************************************
+# *                                                                     *
+# * Copyright (c) 2021 Hakan Seven <hakanseven12@gmail.com>             *
+# *                                                                     *
+# * This program is free software; you can redistribute it and/or modify*
+# * it under the terms of the GNU Lesser General Public License (LGPL)  *
+# * as published by the Free Software Foundation; either version 2 of   *
+# * the License, or (at your option) any later version.                 *
+# * for detail see the LICENCE text file.                               *
+# *                                                                     *
+# * This program is distributed in the hope that it will be useful,     *
+# * but WITHOUT ANY WARRANTY; without even the implied warranty of      *
+# * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the       *
+# * GNU Library General Public License for more details.                *
+# *                                                                     *
+# * You should have received a copy of the GNU Library General Public   *
+# * License along with this program; if not, write to the Free Software *
+# * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307*
+# * USA                                                                 *
+# *                                                                     *
+# ***********************************************************************
+
+'''
+Create a Guide Lines object from FPO.
+'''
+
+import FreeCAD
+import Part
+from pivy import coin
+from freecad.trails import ICONPATH, geo_origin
+from .gl_func import GLFunc
+import copy
+
+
+def create():
+    obj=FreeCAD.ActiveDocument.addObject("App::FeaturePython", "Guidelines")
+    obj.Label = "Guidelines"
+    GuideLines(obj)
+    ViewProviderGuideLines(obj.ViewObject)
+    FreeCAD.ActiveDocument.recompute()
+
+    return obj
+
+
+class GuideLines(GLFunc):
+    """
+    This class is about Guide Lines object data features.
+    """
+
+    def __init__(self, obj):
+        '''
+        Set data properties.
+        '''
+
+        self.Type = 'Trails::Guidelines'
+
+        obj.addProperty(
+            'App::PropertyLink', "Alignment", "Base",
+            "Parent alignment").Alignment = None
+
+        obj.addProperty(
+            "App::PropertyFloatList", "StationList", "Base",
+            "List of stations").StationList = []
+
+        obj.addProperty(
+            "Part::PropertyPartShape", "Shape", "Base",
+            "Object shape").Shape = Part.Shape()
+
+        obj.addProperty(
+            "App::PropertyLength", "RightOffset", "Offset",
+            "Length of right offset").RightOffset = 20000
+
+        obj.addProperty(
+            "App::PropertyLength", "LeftOffset", "Offset",
+            "Length of left offset").LeftOffset = 20000
+
+        obj.Proxy = self
+
+    def onChanged(self, obj, prop):
+        '''
+        Do something when a data property has changed.
+        '''
+        return
+
+    def execute(self, obj):
+        '''
+        Do something when doing a recomputation. 
+        '''
+        if not obj.InList: return
+
+        obj.StationList = obj.InList[0].StationList
+        alignment = obj.getPropertyByName("Alignment")
+        stations = obj.getPropertyByName("StationList")
+
+        if alignment and stations:
+            left_offset = obj.getPropertyByName("LeftOffset")
+            right_offset = obj.getPropertyByName("RightOffset")
+
+            offsets = [left_offset, right_offset]
+
+            # Get GeoOrigin.
+            origin = geo_origin.get()
+            base = copy.deepcopy(origin.Origin)
+            base.z = 0
+
+            obj.Shape = self.get_lines(base, alignment, offsets, stations)
+
+
+class ViewProviderGuideLines:
+    """
+    This class is about Point Group Object view features.
+    """
+
+    def __init__(self, vobj):
+        '''
+        Set view properties.
+        '''
+        vobj.Proxy = self
+
+    def attach(self, vobj):
+        '''
+        Create Object visuals in 3D view.
+        '''
+        # Lines root.
+        self.line_coords = coin.SoGeoCoordinate()
+        self.lines = coin.SoLineSet()
+        self.gl_labels = coin.SoSeparator()
+
+        # Line style.
+        line_color = coin.SoBaseColor()
+        line_color.rgb = (0.0, 1.0, 1.0)
+        line_style = coin.SoDrawStyle()
+        line_style.style = coin.SoDrawStyle.LINES
+        line_style.lineWidth = 2
+
+        # Highlight for selection.
+        highlight = coin.SoType.fromName('SoFCSelection').createInstance()
+        highlight.style = 'EMISSIVE_DIFFUSE'
+        highlight.addChild(line_style)
+        highlight.addChild(self.line_coords)
+        highlight.addChild(self.lines)
+
+        # Surface root.
+        guidelines_root = coin.SoSeparator()
+        guidelines_root.addChild(self.gl_labels)
+        guidelines_root.addChild(line_color)
+        guidelines_root.addChild(highlight)
+        vobj.addDisplayMode(guidelines_root,"Lines")
+
+    def onChanged(self, vobj, prop):
+        '''
+        Update Object visuals when a view property changed.
+        '''
+        pass
+
+    def updateData(self, obj, prop):
+        '''
+        Update Object visuals when a data property changed.
+        '''
+        if prop == "Shape":
+            self.gl_labels.removeAllChildren()
+            shape = obj.getPropertyByName("Shape")
+
+            # Get GeoOrigin.
+            origin = geo_origin.get()
+            base = copy.deepcopy(origin.Origin)
+            base.z = 0
+
+            # Set GeoCoords.
+            geo_system = ["UTM", origin.UtmZone, "FLAT"]
+            self.line_coords.geoSystem.setValues(geo_system)
+
+            points = []
+            line_vert = []
+            for i, wire in enumerate(shape.Wires):
+                font = coin.SoFont()
+                font.size = 3000
+                gl_label = coin.SoSeparator()
+                location = coin.SoTranslation()
+                text = coin.SoAsciiText()
+
+                label = str(round(obj.StationList[i], 2))
+                location.translation = wire.Vertexes[-1].Point
+                text.string.setValues([label])
+                gl_label.addChild(font)
+                gl_label.addChild(location)
+                gl_label.addChild(text)
+                self.gl_labels.addChild(gl_label)
+
+                for vertex in wire.Vertexes:
+                    points.append(vertex.Point.add(base))
+
+                line_vert.append(len(wire.Vertexes))
+
+            self.line_coords.point.values = points
+            self.lines.numVertices.values = line_vert
+
+    def getDisplayModes(self, vobj):
+        '''
+        Return a list of display modes.
+        '''
+        modes=[]
+        modes.append("Lines")
+
+        return modes
+
+    def getDefaultDisplayMode(self):
+        '''
+        Return the name of the default display mode.
+        '''
+        return "Lines"
+
+    def setDisplayMode(self,mode):
+        '''
+        Map the display mode defined in attach with 
+        those defined in getDisplayModes.
+        '''
+        return mode
+
+    def getIcon(self):
+        '''
+        Return object treeview icon.
+        '''
+        return ICONPATH + '/icons/GuideLines.svg'
+
+    def __getstate__(self):
+        """
+        Save variables to file.
+        """
+        return None
+ 
+    def __setstate__(self,state):
+        """
+        Get variables from file.
+        """
+        return None
