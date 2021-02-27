2bc519b1d6a4a53e6d6e631baa6f73daa73c52db
Hakan Seven <hakanseven12@gmail.com>
2021-02-27 10:39:25 +0300

    Add files via upload

---
+++ freecad/trails/geomatics/volume/compute_areas.py
@@ -0,0 +1,67 @@
+# /**********************************************************************
+# *                                                                     *
+# * Copyright (c) 2021 Hakan Seven <hakanseven12@gmail.com>             *
+# *                                                                     *
+# * This program is free software; you can redistribute it and/or modify*
+# * it under the terms of the GNU Lesser General Public License (LGPL)  *
+# * as published by the Free Software Foundation; either version 2 of   *
+# * the License, or (at your option) any later version.                 *
+# * for detail see the LICENCE text file.                               *
+# *                                                                     *
+# * This program is distributed in the hope that it will be useful,     *
+# * but WITHOUT ANY WARRANTY; without even the implied warranty of      *
+# * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the       *
+# * GNU Library General Public License for more details.                *
+# *                                                                     *
+# * You should have received a copy of the GNU Library General Public   *
+# * License along with this program; if not, write to the Free Software *
+# * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307*
+# * USA                                                                 *
+# *                                                                     *
+# ***********************************************************************
+
+import FreeCAD
+import FreeCADGui
+from freecad.trails import ICONPATH
+from .volume_func import VolumeFunc
+
+
+class ComputeAreas(VolumeFunc):
+    """
+    Command to compute areas between surface sections
+    """
+
+    def __init__(self):
+        """
+        Command to create guide lines for selected alignment.
+        """
+        pass
+
+    def GetResources(self):
+        """
+        Return the command resources dictionary
+        """
+        return {
+            'Pixmap': ICONPATH + '/icons/volume.svg',
+            'MenuText': "Compute areas",
+            'ToolTip': "Compute areas between surface sections"
+            }
+
+    def IsActive(self):
+        """
+        Define tool button activation situation
+        """
+        # Check for document
+        if FreeCAD.ActiveDocument:
+            return True
+        return False
+
+    def Activated(self):
+        """
+        Command activation method
+        """
+        # Check for selected object
+        selection = FreeCADGui.Selection.getSelection()
+        self.test(selection)
+        
+FreeCADGui.addCommand('Compute Areas', ComputeAreas())
