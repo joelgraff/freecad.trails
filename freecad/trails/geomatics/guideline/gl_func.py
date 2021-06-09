51d296222daf01e7f90e7e78945366ef690c1d57
Hakan Seven <hakanseven12@gmail.com>
2021-06-09 13:58:58 +0300

    speed up gl drawing

--- freecad/trails/geomatics/guideline/gl_func.py
+++ freecad/trails/geomatics/guideline/gl_func.py
@@ -36,37 +36,11 @@ class GLFunc:
     def __init__(self):
         pass
 
-    def line_orthogonal(self, line, distance, side=''):
-        """
-        Return the orthogonal vector pointing toward the indicated side at the
-        provided position.  Defaults to left-hand side
-        """
-
-        _dir = 1.0
-
-        _side = side.lower()
-
-        if _side in ['r', 'rt', 'right']:
-            _dir = -1.0
-
-        start = line.Start
-        end = line.End
-
-        if (start is None) or (end is None):
-            return None, None
-
-        _delta = end.sub(start).normalize()
-        _left = FreeCAD.Vector(-_delta.y, _delta.x, 0.0)
-
-        _coord = start.add(_delta.multiply(distance*1000))
-
-        return _coord, _left.multiply(_dir)
-
     def get_lines(self, fpoint, alignment, offsets, stations):
         """
         Find the existing Guide Line Clusters group object
         """
-        wire_list = []
+        gls = []
 
         # Get left and right offsets from centerline
         left_offset = offsets[0]
@@ -74,13 +48,9 @@ class GLFunc:
 
         # Computing coordinates and orthoginals for guidelines
         for sta in stations:
-            if hasattr(alignment.Proxy, 'model'):
-                tuple_coord, tuple_vec = alignment.Proxy.model.get_orthogonal( sta, "Left")
-                coord = FreeCAD.Vector(tuple_coord).sub(fpoint)
-                vec = FreeCAD.Vector(tuple_vec)
-
-            else:
-                coord, vec = self.line_orthogonal(alignment, sta, "Left")
+            tuple_coord, tuple_vec = alignment.Proxy.model.get_orthogonal( sta, "Left")
+            coord = FreeCAD.Vector(tuple_coord).sub(fpoint)
+            vec = FreeCAD.Vector(tuple_vec)
 
             left_vec = copy.deepcopy(vec)
             right_vec = copy.deepcopy(vec)
@@ -88,11 +58,7 @@ class GLFunc:
             left_side = coord.add(left_vec.multiply(left_offset))
             right_side = coord.add(right_vec.negative().multiply(right_offset))
 
-            left_line = Part.LineSegment(left_side, coord)
-            right_line = Part.LineSegment(coord, right_side)
-
             # Generate guide line object and add to cluster
-            shape = Part.Shape([left_line,right_line])
-            wire_list.append(Part.Wire(shape.Edges))
+            gls.append(Part.makePolygon([left_side, coord, right_side]))
 
-        return Part.makeCompound(wire_list)
+        return Part.makeCompound(gls)
