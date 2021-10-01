542ac4a3e0a3d449549b08045d1e95fd6097bf8d
Hakan Seven <hakanseven12@gmail.com>
2021-10-01 10:13:23 +0300

    import alignment task-new item selection method

--- freecad/trails/design/project/tasks/alignment/import_alignment_task.py
+++ freecad/trails/design/project/tasks/alignment/import_alignment_task.py
@@ -28,7 +28,7 @@ import Mesh
 from PySide import QtGui, QtCore
 
 from freecad.trails import resources, geo_origin
-from . import import_xml_subtask
+from .import_xml_subtask import ImportXmlSubtask
 from ...support import utils
 from ....alignment import horizontal_alignment
 from .....geomatics.surface import surface
@@ -43,12 +43,66 @@ class ImportAlignmentTask:
     Import horizontal alignments from various file formats
     """
     def __init__(self):
+        """
+        Constructor
+        """
+        ui_path = resources.__path__[0] + '/ui/import_landxml_panel.ui'
+        self.form = FreeCADGui.PySideUic.loadUi(ui_path)
+        self.form.tb_browse.clicked.connect(self.choose_file)
+        self.form.le_filename.textChanged.connect(self.examine_file)
 
-        self.ui_path = resources.__path__[0] + '/ui/'
-        self.ui = self.ui_path + 'import_alignment_task_panel.ui'
+    def choose_file(self):
+        """
+        Open the file picker dialog and open the file that the user chooses
+        """
+        parameter = FreeCAD.ParamGet("User parameter:BaseApp/Preferences/General")
+        path = parameter.GetString("FileOpenSavePath")
+        filters = self.form.tr('LandXML files (*.xml)')
 
-        self.form = None
-        self.subtask = None
+        file_name = QtGui.QFileDialog.getOpenFileName(
+                        self.form, 'Select a LandXML File',
+                        path, 'LandXML files (*.xml)', filters)
+
+        self.form.le_filename.setText(file_name[0])
+
+    def examine_file(self):
+        """
+        Examine the file path indicated in the QLineEdit, determine the type,
+        and pass parsing on to the appropriate module
+        """
+        filename = 'import_alignment_task_xml_subpanel.ui'
+        filepath = self.form.le_filename.text()
+        subpanel = FreeCADGui.PySideUic.loadUi(resources.__path__[0] + '/ui/' + filename, None)
+        self.subtask = ImportXmlSubtask(subpanel, filepath)
+
+        try:
+            self.data = self.subtask.parser.import_file(filepath)
+            if self.subtask.parser.errors:
+                for error in self.subtask.parser.errors:
+                    print(error)
+
+            model = QtGui.QStandardItemModel()
+            self.form.tv_items.setModel(model)
+
+            for name, group in self.data.items():
+                group_widget = QtGui.QStandardItem(name)
+                group_widget.setCheckable(True)
+                group_widget.setCheckState(QtCore.Qt.Checked)
+                model.appendRow(group_widget)
+
+                for i in group:
+                    item_widget = QtGui.QStandardItem(i)
+                    item_widget.setCheckable(True)
+                    item_widget.setCheckState(QtCore.Qt.Checked)
+                    group_widget.appendRow(item_widget)
+
+        except OSError:
+            dialog = QtGui.QMessageBox(
+                QtGui.QMessageBox.Critical, 'Unable to open file ', filepath)
+
+            dialog.setWindowModality(QtCore.Qt.ApplicationModal)
+            dialog.exec_()
+            return
 
     def accept(self):
         """
@@ -127,99 +181,3 @@ class ImportAlignmentTask:
         Clicked callback
         """
         pass
-
-    def choose_file(self):
-        """
-        Open the file picker dialog and open the file
-        that the user chooses
-        """
-        parameter = FreeCAD.ParamGet("User parameter:BaseApp/Preferences/General")
-        path = parameter.GetString("FileOpenSavePath")
-
-        filters = self.form.tr(
-            'All files (*.*);; CSV files (*.csv);; LandXML files (*.xml)'
-        )
-
-        selected_filter = self.form.tr('LandXML files (*.xml)')
-
-        file_name = QtGui.QFileDialog.getOpenFileName(
-            self.form, 'Select File', path, filters, selected_filter
-        )
-
-        if not file_name[0]:
-            return
-
-        self.form.file_path.setText(file_name[0])
-
-    def examine_file(self):
-        """
-        Examine the file path indicated in the QLineEdit, determine the type,
-        and pass parsing on to the appropriate module
-        """
-
-        file_path = self.form.file_path.text()
-        filename, extension = os.path.splitext(file_path)
-        stream = None
-
-        try:
-            stream = open(file_path)
-            stream.close()
-
-        except OSError:
-            dialog = QtGui.QMessageBox(
-                QtGui.QMessageBox.Critical, 'Unable to open file ', file_path
-            )
-            dialog.setWindowModality(QtCore.Qt.ApplicationModal)
-            dialog.exec_()
-            return
-
-        filename = 'import_alignment_task_xml_subpanel.ui'
-
-        if 'csv' in extension:
-            filename = 'import_alignment_task_csv_subpanel.ui'
-
-        subpanel = FreeCADGui.PySideUic.loadUi(self.ui_path + filename, None)
-
-        #ensure any existing subpanels are removed
-        itm_count = self.form.layout().count()
-
-        while itm_count > 1:
-
-            item = self.form.layout().itemAt(itm_count - 1)
-            self.form.layout().removeItem(item)
-            itm_count = self.form.layout().count()
-
-        self.form.layout().addWidget(subpanel)
-
-        if 'xml' in extension:
-            self.subtask = import_xml_subtask.create(subpanel, file_path)
-
-        #elif '.csv' in extension:
-        #    self.subtask = ImportCsvSubtask.create(subpanel, file_path)
-
-    def setup(self):
-        """
-        Initiailze the task window and controls
-        """
-        _mw = utils.getMainWindow()
-
-        form = _mw.findChild(QtGui.QWidget, 'TaskPanel')
-
-        form.file_path = form.findChild(QtGui.QLineEdit, 'filename')
-        form.pick_file = form.findChild(QtGui.QToolButton, 'pick_file')
-        form.pick_file.clicked.connect(self.choose_file)
-        form.file_path.textChanged.connect(self.examine_file)
-
-        self.form = form
-
-    def macro_load(self, file_path):
-        """
-        Allow loading a file via macro / automation
-        """
-
-        filename = 'import_alignment_task_xml_subpanel.ui'
-        subpanel = FreeCADGui.PySideUic.loadUi(self.ui_path + filename, None)
-
-        self.subtask = import_xml_subtask.create(subpanel, file_path)
-
-        self.accept()
